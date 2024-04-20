bc4dd81039525e5a353c23c45646b4a8f4a847fc
johnclavss <johnclavss@yahoo.com>
2024-04-20 22:03:39 +0800

    Update explore-boxitem.jsx

--- frontend/src/components/explorepage/explore-boxitem.jsx
+++ frontend/src/components/explorepage/explore-boxitem.jsx
@@ -129,14 +129,14 @@ export default function ExploreItemBox() {
                 <Typography fontFamily='Poppins'>
                   ISBN: #1234567890
                 </Typography>
-                <Typography variant='h6'mt={1} fontWeight='bold'>
+                <Typography variant='h6'mt={1} color='#50623A' fontWeight='bold'>
                 About the Book
                 </Typography>
                 <Divider sx={{mb:.5,mt:.5}}/>
                 <Typography sx={{ textAlign: 'justify', textIndent: '5em',fontSize:'15px' }}>
                   Harry Potter and the Philosopher's Stone is a fantasy novel written by British author J. K. Rowling. The first novel in the Harry Potter series and Rowling's debut novel, it follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry. Harry makes close friends and a few enemies during his first year at the school and with the help of his friends, Ron Weasley and Hermione Granger, he faces an attempted comeback by the dark wizard Lord Voldemort, who killed Harry's parents, but failed to kill Harry when he was just 15 months old.
                 </Typography>
-                <Typography variant='h6'mt={1} fontWeight='bold'>
+                <Typography variant='h6'mt={1} color='#50623A' fontWeight='bold'>
                  Book Contents
                 </Typography>
                 <Divider sx={{mb:.5,mt:.5}}/>
