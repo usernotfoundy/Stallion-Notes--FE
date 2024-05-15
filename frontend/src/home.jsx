87469fb34eeee8c849c74809b83584def86023f5
Emman Adonay <63085286+usernotfoundy@users.noreply.github.com>
2024-05-15 22:53:42 +0800

    Update home.jsx

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -116,45 +116,7 @@ import FeedbackButton from "./components/feedbackbtn";
 
 
 const HomePage = () => {
-  /*
-     # Authentication APIs
-      const VIEW_PROFILE_API_URL = 'http://127.0.0.1:8000/view-users/'
-      const REGISTER_URL = 'http://127.0.0.1:8000/register/'
-      const LOGIN_URL = 'http://127.0.0.1:8000/login/'
-      const UPDATE_PROFILE_URL = 'http://127.0.0.1:8000/update-user/'
-      const VIEW_PROFILE_API_URL = 'http://127.0.0.1:8000/view-profile/'
-  
-      # College & Course APIs
-      const CREATE_COLLEGE_API_URL = 'http://127.0.0.1:8000/create-college/'
-      const VIEW_COLLEGE_API_URL = 'http://127.0.0.1:8000/view-college/'
-      const CREATE_COURSE_API_URL = 'http://127.0.0.1:8000/create-course/'
-      const VIEW_COURSE_API_URL = 'http://127.0.0.1:8000/view-course/'
-  
-      # Book Management APIs
-      const VIEW_BOOKS_API_URL = 'http://127.0.0.1:8000/view-books/'
-      const UPLOAD_BOOKS_API_URL = 'http://127.0.0.1:8000/create-book/'
-      const UPDATE_BOOKS_API_URL = 'http://127.0.0.1:8000/update-book/'
-      const DELETE_BOOKS_API_URL = 'http://127.0.0.1:8000/delete-book/'
-      const SEARCH_BOOKS_URL = 'http://127.0.0.1:8000/search-book/'
-      const EXPLORE_API_URL = 'http://127.0.0.1:8000/explore-books/';
-      const GENRE_API_URL = 'http://127.0.0.1:8000/get-genre/'
-  
-      # Cart Management APIs
-      const ADD_CART_API_URL = 'http://127.0.0.1:8000/add-cart/'
-      const VIEW_CART_API_URL = 'http://127.0.0.1:8000/view-cart/'
-      const VIEW_POSTS_API_URL = 'http://127.0.0.1:8000/view-posts/';
 
-      #Rating Management APIs
-      path('rate-app/', RatingCreateView.as_view(), name='rate-app'),
-      const RATE_API_URL = 'http://127.0.0.1:8000/rate-app/'
-      path('view-ratings/', RatingAPIView.as_view(), name='view-ratings'),
-
-      #Purchase Management APIs
-      path('purchase-book/', PurchaseCreateAPIView.as_view(), name='purchase-book'),
-      path('view-purchase/', PurchaseListAPIView.as_view(), name='view-purchase'),
-      path('update-purchased-book/', UpdatePurchasedBookAPIView.as_view(), name='update-purchased-book'),
-
-  */
   const navigate = useNavigate('/');
   useEffect(() => {
     // Check if authToken is available in localStorage
