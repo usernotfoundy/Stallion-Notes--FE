89e34c8be504798b3b82d94c70145af3c387268f
Emzkie The Great <63085286+usernotfoundy@users.noreply.github.com>
2024-06-14 22:46:02 +0800

    resolved merge conflicts
    fixed conflicts and updated code version.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,38 +1,28 @@
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
-
-// // // https://vitejs.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // })
-
-
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
-// // import cors from '@koa/cors'
 
+// // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   server: {
-//     // middleware: [cors()], // Use CORS middleware
-//     fs: {
-//       // Specify directories to allow serving files from
-//       allow: [
-//         // Allow serving files from the entire project
-//         '.',
-//         // Specifically allow the src directory, adjust the path as necessary
-//         './src',
-//       ],
-//     },
-//   },
 // })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// import cors from '@koa/cors'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
+  server: {
+    // middleware: [cors()], // Use CORS middleware
+    fs: {
+      // Specify directories to allow serving files from
+      allow: [
+        // Allow serving files from the entire project
+        '.',
+        // Specifically allow the src directory, adjust the path as necessary
+        './src',
+      ],
+    },
   },
-  base: '/',  // Ensure the base path is correctly set
-});
+})
